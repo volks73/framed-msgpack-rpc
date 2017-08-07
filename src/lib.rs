5c777e75e980c635c20a48cf237d937ebe44a3d2
Christopher Field <cfield2@gmail.com>
2017-08-06 20:27:18 -0400

    Add library imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,33 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+//! Frame a MessagePack-RPC message (payload) from a transport with the total message length prefixed as
+//! a 32-bit unsigned integer encoded in four (4) bytes.
+//!
+//! # Getting Started
+//!
+//! This can be used with the tokio-proto crate.
+//!
+//! ```
+//! use framed_msgpack_rpc::Codec;
+//! use tokio_io::{AsyncRead, AsyncWrite};
+//! use tokio_io::codec::Framed;
+//! use tokio_proto::pipeline::ServerProto;
+//!
+//! struct FramedMsgpackRpcProto;
+//! 
+//! impl<T: AsyncRead + AsyncWrite + 'static> ServerProto<T> for FramedMsgpackRpcProto {
+//!    type Request = Value;
+//!    type Response = Value;
+//!    type Transport = Framed<T, Codec>
+//!    type BindTransport = Result<Self::Transport, io::Error>;
+//!
+//!    fn bind_transport(&self, io: T) -> Self::BindTransport {
+//!        Ok(io.framed(Codec::new()))
+//!    }
+//! }
+//! ```
+
+extern crate bytes;
+extern crate framed_msgpack;
+extern crate futures;
+extern crate rmpv;
+extern crate tokio_io;
+
